7b9c99005ba74411fa39fd800025415cf865c814
Victoria Ransom <victoriamransom@gmail.com>
2023-10-23 12:02:09 -0700

    Add project files.

---
+++ HTMLsubmission/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function newFont() {
+    document.getElementById("greeting").style.fontWeight = "700";
+}
+
+function oldFont() {
+    document.getElementById("greeting").style.fontWeight = "100";
+}
+
